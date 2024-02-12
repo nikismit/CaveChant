768258523d17ddc06e8bafa2e8a9dae189474c89
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-02-12 14:06:44 +0100

    alpha

--- Assets/_Scripts/Generator.cs
+++ Assets/_Scripts/Generator.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Branch
+public class Passage
 {
     public Vector3 start;
     public Vector3 end;
     public Vector3 direction;
-    public Branch parent;
-    public List<Branch> children = new List<Branch>();
+    public Passage parent;
+    public List<Passage> children = new List<Passage>();
     public List<Vector3> attractors = new List<Vector3>();
 
-    public Branch(Vector3 start, Vector3 end, Vector3 direction, Branch parent = null) 
+    public Passage(Vector3 start, Vector3 end, Vector3 direction, Passage parent = null) 
     {
         this.start = start;
         this.end = end;
@@ -21,119 +21,125 @@ public class Branch
 
 public class Generator : MonoBehaviour 
 {
-    [Header("Generation parameters")]
-    public Vector3 startPosition = new Vector3(0, -4, 0);
-    public int nbAttractors = 300;
-    public float radius = 5;
-    public float branchLength = 0.4f;
+    public Vector3 entrance = new Vector3(0, -4, 0);
+    public int nodesAmount = 300;
+    public int nodesLeft = 20;
+    public float caveRadius = 5;
+    public float passageLength = 0.4f;
     public float timeBetweenIterations = 0.2f;
     public float attractionRange = 1;
-    public float killRange = 0.5f;
+    public float killRange = 0.6f;
     public float randomGrowth = 0.1f;
 
-    private List<Vector3> attractors = new List<Vector3>();
-    private List<int> activeAttractors = new List<int>();
-    private Branch firstBranch;
-    private List<Branch> branches = new List<Branch>();
-    private List<Branch> extremities = new List<Branch>();
+    private List<Vector3> nodes = new List<Vector3>();
+    private List<int> activeNodes = new List<int>();
+    private Passage firstPassage;
+    private List<Passage> passages = new List<Passage>();
+    private List<Passage> extremities = new List<Passage>();
     private float timeSinceLastIteration = 0f;
 
     private void Start()
     {
         // create nodes
-        GenerateAttractors(nbAttractors, radius / 2);
+        GenerateNodes(nodesAmount, caveRadius / 2);
 
-        // create starting point
-        firstBranch = new Branch(startPosition, startPosition + new Vector3(0, branchLength, 0), new Vector3(0, 1, 0));
-        branches.Add(firstBranch);
-        extremities.Add(firstBranch);
+        // create cave entrance passage
+        firstPassage = new Passage(entrance, entrance + new Vector3(0, passageLength, 0), new Vector3(0, 1, 0));
+        passages.Add(firstPassage);
+        extremities.Add(firstPassage);
     }
 
     private void Update() 
     {
+        if (nodes.Count <= nodesLeft)
+        {
+            extremities.Clear();
+            nodes.Clear();
+        }
+
         timeSinceLastIteration += Time.deltaTime;
 
         if (timeSinceLastIteration > timeBetweenIterations)
         {
             timeSinceLastIteration = 0f;
 
-            for (int i = attractors.Count - 1; i >= 0; i--)
+            for (int i = nodes.Count - 1; i >= 0; i--)
             {
-                for (int j = 0; j < branches.Count; j++) 
+                for (int j = 0; j < passages.Count; j++) 
                 {
-                    if (Vector3.Distance(branches[j].end, attractors[i]) < killRange) 
+                    if (Vector3.Distance(passages[j].end, nodes[i]) < killRange) 
                     {
-                        attractors.Remove(attractors[i]);
-                        nbAttractors--;
+                        nodes.Remove(nodes[i]);
+                        nodesAmount--;
                         break;
                     }
                 }
             }
 
-            if (attractors.Count > 0)
+            if (nodes.Count > 0)
             {
-                activeAttractors.Clear();
+                activeNodes.Clear();
 
-                for (int i = 0; i < branches.Count; i++) branches[i].attractors.Clear();
+                for (int i = 0; i < passages.Count; i++) passages[i].attractors.Clear();
 
-                for (int ia = 0; ia < attractors.Count; ia++) 
+                for (int ia = 0; ia < nodes.Count; ia++) 
                 {
                     float min = 999999f;
-                    Branch closest = null;
+                    Passage closest = null;
 
-                    for (int j = 0; j < branches.Count; j++) 
+                    for (int j = 0; j < passages.Count; j++) 
                     {
-                        float d = Vector3.Distance(branches[j].end, attractors[ia]);
+                        float d = Vector3.Distance(passages[j].end, nodes[ia]);
                         if (d < attractionRange && d < min) 
                         {
                             min = d;
-                            closest = branches[j];
+                            closest = passages[j];
                         }
                     }
 
                     if (closest != null) 
                     {
-                        closest.attractors.Add(attractors[ia]);
-                        activeAttractors.Add(ia);
+                        closest.attractors.Add(nodes[ia]);
+                        activeNodes.Add(ia);
                     }
                 }
 
-                if (activeAttractors.Count != 0) 
+                if (activeNodes.Count != 0) 
                 {
                     extremities.Clear();
-                    List<Branch> newBranches = new List<Branch>();
+                    List<Passage> newBranches = new List<Passage>();
 
-                    for (int i = 0; i < branches.Count; i++) 
+                    for (int i = 0; i < passages.Count; i++) 
                     {
-                        if (branches[i].attractors.Count > 0) 
+                        if (passages[i].attractors.Count > 0) 
                         {
                             Vector3 dir = new Vector3(0, 0, 0);
-                            for (int j = 0; j < branches[i].attractors.Count; j++) dir += (branches[i].attractors[j] - branches[i].end).normalized;
-                            dir /= branches[i].attractors.Count;
+                            for (int j = 0; j < passages[i].attractors.Count; j++) dir += (passages[i].attractors[j] - passages[i].end).normalized;
+                            dir /= passages[i].attractors.Count;
                             dir += RandomGrowthVector();
                             dir.Normalize();
-                            Branch nb = new Branch(branches[i].end, branches[i].end + dir * branchLength, dir, branches[i]);
-                            branches[i].children.Add(nb);
-                            newBranches.Add(nb);
-                            extremities.Add(nb);
+                            Passage passage = new Passage(passages[i].end, passages[i].end + dir * passageLength, dir, passages[i]);
+                            passages[i].children.Add(passage);
+                            newBranches.Add(passage);
+                            extremities.Add(passage);
                         } 
-                        else if (branches[i].children.Count == 0) extremities.Add(branches[i]);
+                        else if (passages[i].children.Count == 0) extremities.Add(passages[i]);
                     }
 
-                    branches.AddRange(newBranches);
+                    passages.AddRange(newBranches);
                 } 
                 else 
                 {
                     for (int i = 0; i < extremities.Count; i++) 
                     {
-                        Branch e = extremities[i];
-                        Vector3 start = e.end;
-                        Vector3 dir = e.direction + RandomGrowthVector();
-                        Vector3 end = e.end + dir * branchLength;
-                        Branch nb = new Branch(start, end, dir, e);
-                        e.children.Add(nb);
-                        branches.Add(nb);
-                        extremities[i] = nb;
+                        Passage extremity = extremities[i];
+                        Vector3 start = extremity.end;
+                        Vector3 dir = extremity.direction + RandomGrowthVector();
+                        Vector3 end = extremity.end + dir * passageLength;
+                        Passage passage = new Passage(start, end, dir, extremity);
+                        extremity.children.Add(passage);
+                        passages.Add(passage);
+                        extremities[i] = passage;
                     }
                 }
             }
@@ -148,9 +154,9 @@ public class Generator : MonoBehaviour
         return pt * randomGrowth;
     }
 
-    private void GenerateAttractors(int n, float r) 
+    private void GenerateNodes(int n, float r) 
     {
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
         {
             float radius = Random.Range(0f, 1f);
             radius = Mathf.Pow(Mathf.Sin(radius * Mathf.PI / 2f), 0.8f);
@@ -159,28 +165,31 @@ public class Generator : MonoBehaviour
             float theta = Random.Range(0f, Mathf.PI * 2f);
             Vector3 pt = new Vector3(radius * Mathf.Cos(theta) * Mathf.Sin(alpha), radius * Mathf.Sin(theta) * Mathf.Sin(alpha), radius * Mathf.Cos(alpha));
             pt += transform.position;
-            attractors.Add(pt);
+            nodes.Add(pt);
         }
     }
 
     private void OnDrawGizmos() 
     {
-        for (int i = 0; i < branches.Count; i++) 
+        for (int i = 0; i < nodes.Count; i++)
         {
-            Branch b = branches[i];
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(b.start, b.end);
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawSphere(b.end, 0.05f);
-            Gizmos.DrawSphere(b.start, 0.05f);
+            var node = nodes[i];
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(new Vector3(node.x, node.y, node.z), 0.05f);
+        }
+        
+        for (int i = 0; i < passages.Count; i++)
+        {
+            Passage passage = passages[i];
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(passage.start, passage.end);
         }
 
-        for (int i = 0; i < attractors.Count; i++) 
+        for (int i = 0; i < extremities.Count; i++)
         {
-            var a = attractors[i];
-            Vector3 pos = new Vector3(a.x, a.y, a.z);
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(pos, 0.05f);
+            Passage extremity = extremities[i];
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(extremity.start, extremity.end);
         }
     }
 }
