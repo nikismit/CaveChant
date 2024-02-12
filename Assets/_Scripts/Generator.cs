c4e992db161e4d6fd03b9fe68f14043ce06576b6
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-02-12 11:06:00 +0100

    initial cave system generation

---
+++ Assets/_Scripts/Generator.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour 
+{
+
+    public class Branch 
+    {
+        public Vector3 start;
+        public Vector3 end;
+        public Vector3 direction;
+        public Branch parent;
+        public float size;
+        public float lastSize;
+        public List<Branch> children = new List<Branch>();
+        public List<Vector3> attractors = new List<Vector3>();
+        public int distanceFromRoot = 0;
+        public bool grown = false;
+
+        public Branch(Vector3 start, Vector3 end, Vector3 direction, Branch parent = null) 
+        {
+            this.start = start;
+            this.end = end;
+            this.direction = direction;
+            this.parent = parent;
+        }
+    }
+
+    [Header("Generation parameters")]
+    [Range(0, 3000)] public int nbAttractors = 400;
+    [Range(0f, 10f)] public float radius = 5f;
+    public Vector3 startPosition = new Vector3(0, 0, 0);
+    [Range(0f, 0.5f)] public float branchLength = 0.2f;
+    [Range(0f, 1f)] public float timeBetweenIterations = 0.5f;
+    [Range(0f, 3f)] public float attractionRange = 0.1f;
+    [Range(0f, 2f)] public float killRange = 0.5f;
+    [Range(0f, 0.2f)] public float randomGrowth = 0.1f;
+
+    private List<Vector3> attractors = new List<Vector3>();
+    private List<int> activeAttractors = new List<int>();
+    private Branch firstBranch;
+    private List<Branch> branches = new List<Branch>();
+    private List<Branch> extremities = new List<Branch>();
+    private float timeSinceLastIteration = 0f;
+
+    private void Start() 
+    {
+        GenerateAttractors(nbAttractors, radius);
+        firstBranch = new Branch(startPosition, startPosition + new Vector3(0, branchLength, 0), new Vector3(0, 1, 0));
+        branches.Add(firstBranch);
+        extremities.Add(firstBranch);
+    }
+
+    private void Update() 
+    {
+        timeSinceLastIteration += Time.deltaTime;
+
+        if (timeSinceLastIteration > timeBetweenIterations) 
+        {
+            timeSinceLastIteration = 0f;
+
+            for (int i = 0; i < extremities.Count; i++) 
+            {
+                extremities[i].grown = true;
+            }
+
+            for (int i = attractors.Count - 1; i >= 0; i--) 
+            {
+                for (int j = 0; j < branches.Count; j++) 
+                {
+                    if (Vector3.Distance(branches[j].end, attractors[i]) < killRange) 
+                    {
+                        attractors.Remove(attractors[i]);
+                        nbAttractors--;
+                        break;
+                    }
+                }
+            }
+
+            if (attractors.Count > 0) 
+            {
+                activeAttractors.Clear();
+                for (int i = 0; i < branches.Count; i++) 
+                {
+                    branches[i].attractors.Clear();
+                }
+
+                for (int ia = 0; ia < attractors.Count; ia++) 
+                {
+                    float min = 999999f;
+                    Branch closest = null;
+
+                    for (int j = 0; j < branches.Count; j++) 
+                    {
+                        float d = Vector3.Distance(branches[j].end, attractors[ia]);
+                        if (d < attractionRange && d < min) 
+                        {
+                            min = d;
+                            closest = branches[j];
+                        }
+                    }
+
+                    if (closest != null) 
+                    {
+                        closest.attractors.Add(attractors[ia]);
+                        activeAttractors.Add(ia);
+                    }
+                }
+
+                if (activeAttractors.Count != 0) 
+                {
+                    extremities.Clear();
+                    List<Branch> newBranches = new List<Branch>();
+
+                    for (int i = 0; i < branches.Count; i++) 
+                    {
+                        if (branches[i].attractors.Count > 0) 
+                        {
+                            Vector3 dir = new Vector3(0, 0, 0);
+
+                            for (int j = 0; j < branches[i].attractors.Count; j++) 
+                            {
+                                dir += (branches[i].attractors[j] - branches[i].end).normalized;
+                            }
+
+                            dir /= branches[i].attractors.Count;
+                            dir += RandomGrowthVector();
+                            dir.Normalize();
+
+                            Branch nb = new Branch(branches[i].end, branches[i].end + dir * branchLength, dir, branches[i]);
+                            nb.distanceFromRoot = branches[i].distanceFromRoot + 1;
+                            branches[i].children.Add(nb);
+                            newBranches.Add(nb);
+                            extremities.Add(nb);
+                        } 
+                        else 
+                        {
+                            if (branches[i].children.Count == 0) 
+                            {
+                                extremities.Add(branches[i]);
+                            }
+                        }
+                    }
+
+                    branches.AddRange(newBranches);
+                } 
+                else 
+                {
+                    for (int i = 0; i < extremities.Count; i++) 
+                    {
+                        Branch e = extremities[i];
+                        Vector3 start = e.end;
+                        Vector3 dir = e.direction + RandomGrowthVector();
+                        Vector3 end = e.end + dir * branchLength;
+                        Branch nb = new Branch(start, end, dir, e);
+                        e.children.Add(nb);
+                        branches.Add(nb);
+                        extremities[i] = nb;
+                    }
+                }
+            }
+        }
+    }
+
+    private Vector3 RandomGrowthVector() 
+    {
+        float alpha = Random.Range(0f, Mathf.PI);
+        float theta = Random.Range(0f, Mathf.PI * 2f);
+        Vector3 pt = new Vector3(
+            Mathf.Cos(theta) * Mathf.Sin(alpha),
+            Mathf.Sin(theta) * Mathf.Sin(alpha),
+            Mathf.Cos(alpha)
+        );
+        return pt * randomGrowth;
+    }
+
+    private void GenerateAttractors(int n, float r) 
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            float radius = Random.Range(0f, 1f);
+            radius = Mathf.Pow(Mathf.Sin(radius * Mathf.PI / 2f), 0.8f);
+            radius *= r;
+            float alpha = Random.Range(0f, Mathf.PI);
+            float theta = Random.Range(0f, Mathf.PI * 2f);
+            Vector3 pt = new Vector3(
+                radius * Mathf.Cos(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Sin(theta) * Mathf.Sin(alpha),
+                radius * Mathf.Cos(alpha)
+            );
+            pt += transform.position;
+            attractors.Add(pt);
+        }
+    }
+
+    private void OnDrawGizmos() 
+    {
+        for (int i = 0; i < branches.Count; i++) 
+        {
+            Branch b = branches[i];
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(b.start, b.end);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(b.end, 0.05f);
+            Gizmos.DrawSphere(b.start, 0.05f);
+        }
+
+        for (int i = 0; i < attractors.Count; i++) 
+        {
+            var a = attractors[i];
+            Vector3 pos = new Vector3(a.x, a.y, a.z);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(pos, 0.05f);
+        }
+    }
+}
