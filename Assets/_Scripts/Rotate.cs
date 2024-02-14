dafb54dc08602ab6c25b8fd0abf4a7702382c74b
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-02-14 17:46:49 +0100

    added a rotating script to the cave visualization

---
+++ Assets/_Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, speed * Time.deltaTime * 20, 0);
+    }
+}
